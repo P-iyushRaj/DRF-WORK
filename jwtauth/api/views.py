6a4e80efaa39f172534ba001e871d601998fb117
piyush <piyush@gmail.com>
2021-03-10 18:55:43 +0530

    sign up added

--- jwtauth/api/views.py
+++ jwtauth/api/views.py
@@ -1,23 +1,29 @@
-from django.shortcuts import render
-from .models import Student
-from .serializer import StudentSerializer
-
-from rest_framework import viewsets
+from rest_framework import generics, permissions, viewsets
+from rest_framework.response import Response
+from knox.models import AuthToken
+from .serializers import UserSerializer, RegisterSerializer, StudentSerializer
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
+from .models import Student
 
-class StudentModelViewSet(viewsets.ModelViewSet):
+# Register API
+class RegisterAPI(generics.GenericAPIView):
+    serializer_class = RegisterSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        return Response({
+        "user": UserSerializer(user, context=self.get_serializer_context()).data,
+        "token": AuthToken.objects.create(user)[1]
+        })
+
+class Studentview(viewsets.ViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]
 
-'''
-# read only ( list and retrieve) operations in this two lines using ModelViewSet
-class StudentReadOnlyModelViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = Student.objects.all()
-    serializer_class = StudentSerializer
-    authentication_classes = [JWTAuthentication]
-    permission_classes = [IsAuthenticated]
-'''
+
     
--- jwtauth/jwtauth/urls.py
+++ jwtauth/jwtauth/urls.py
@@ -10,30 +10,14 @@ router = DefaultRouter()
 
 #register viewset using router
 # url http://127.0.0.1:8000/studentapi
-router.register('studentapi', views.StudentModelViewSet,
+router.register('studentapi', views.Studentview,
 basename='Student')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(router.urls)),
-    #path('auth/', include('rest_framework.urls', namespace = 'rest_framework')),
 
-    path('gettoken/',TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    #http POST http://127.0.0.1:8000/gettoken/ username="piyush" password="123456"
-    path('refreshtoken/',TokenRefreshView.as_view(), name='token_refresh'),
-    #http POST http://127.0.0.1:8000/refreshtoken/ refresh"refresh token here"
-    path('verifytoken/',TokenVerifyView.as_view(), name='token_verify'),
-    #http POST http://127.0.0.1:8000/verifytoken/ token="eyJ0e"
+    path('api/register/', views.RegisterAPI.as_view(), name='register'),
+    path('', include(router.urls))
 
 ]
-
-#token valid for 5min
-#refreshtoken valid for 1day
-
-#get data
-#http http://127.0.0.1:8000/studentapi/ 'Authorization:Bearer ____token'
-#incert data
-#http -f POST http://127.0.0.1:8000/studentapi/ name="ram" roll=12 city="bhagalpur" 'Authorization:Bearer _________token' 
- 
-
 
