0e4ae6c177d85848e206fa468475687692228786
piyush raj <yourgitemail@example.com>
2021-03-22 08:57:35 +0530

    tweet api changed

--- tweet/app/views.py
+++ tweet/app/views.py
@@ -10,12 +10,20 @@ from .twitter import set_inactive, set_active, save_to_db
 
 from rest_framework.decorators import api_view, renderer_classes
 from rest_framework.renderers import JSONRenderer
-
+from time import sleep
 
 
 class TweetListS(ListAPIView):
     queryset = Tweet.objects.order_by('-published_date')
     serializer_class = TweetSerializer
+
+
+
+from rest_framework.decorators import api_view, renderer_classes
+from rest_framework.renderers import JSONRenderer
+from time import sleep
+from rest_framework.response import Response
+from rest_framework import status
 
 def tweet_set_inactive(request, pk):
     set_inactive(pk)
@@ -26,9 +34,11 @@ def tweet_set_active(request, pk):
     set_active(pk)
     return Response({'msg':'tweet_set_active'}, status=status.HTTP_201_CREATED)
 
+
 @api_view(('GET',))
 @renderer_classes(( JSONRenderer,))
 def tweet_fetch(request):
     save_to_db()
     return Response({'msg':'tweet fetched'}, status=status.HTTP_201_CREATED)
+
 
